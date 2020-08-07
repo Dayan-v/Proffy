8b951a15a1f2719ce62730e82251581b3f8f87da
Dayane Vieira <day.ane.v@hotmail.com>
2020-08-06 22:55:28 -0300

    Third day

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,30 +1,48 @@
 import React from 'react';
 import whatsAppIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+	avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+	whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+    function createNewConnection(){
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars1.githubusercontent.com/u/59886164?s=460&u=e501f707558dfa867b12823b3b3643364b6366c9&v=4" />
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Dayane Vieira</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
             <p>
-                Entusiasta das melhores tecnologias de química avançada.
-                <br /> <br />
-                Apaixonada por explodir coisas em laboratório.
+                {teacher.bio}
             </p>
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsAppIcon} alt= "WhatsApp" />
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
